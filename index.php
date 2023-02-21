02e51d955d798988bacbd2bad58992785bf33be8
Poisss <averinartem2003@gmail.com>
2023-02-21 19:37:12 +0500

    каркас

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Не знаешь где найти расписание? Это печально">
+		<meta name="keywords" content="Расписание мпк, Расписание, мпк">
+		<meta name="Author" content="">
+		<meta name="Copyright" content="">
+		<meta name="Address" content="мпк">
+		<link rel="shortcut icon" href="">
+        <title>PC2035</title>
+        <link rel="stylesheet" href="css.css">
+    </head>
+    <body>
+        <div id="app">
+            <div class="container">
+                <header>
+                    1
+                </header>
+                <main>
+                    1
+                </main>
+                <footer>
+                    1
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
